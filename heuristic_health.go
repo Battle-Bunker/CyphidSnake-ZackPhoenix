567178c3153c83e8b62eaf57f39e454c78adaa5c
ZackBudai <zwatermelon@proton.me>
2024-09-30 22:31:01 +1000

    Update heuristic_health.go

--- heuristic_health.go
+++ heuristic_health.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/ZackBudai/cyphid-snake/agent"
+	"github.com/Battle-Bunker/cyphid-snake/agent"
 )
 
 // heuristicHealth calculates the sum of health for all snakes in your team,
